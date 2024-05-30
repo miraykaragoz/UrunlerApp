1caea85f857fc0d3a56131fea174edd16b77ef30
Miray Karagöz <mmiraykaragoz@hotmail.com>
2024-05-30 15:27:23 +0300

    Add project files.

---
+++ Models/EklemeModel.cs
@@ -0,0 +1,11 @@
+﻿using System.Diagnostics.Eventing.Reader;
+
+namespace UrunlerApp.Models
+{
+    public class EklemeModel
+    {
+        public string? UrunAdi { get; set; }
+        public int? Fiyat { get; set; }
+        public int? Stok { get; set; }
+    }
+}
